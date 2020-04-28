33bb048a41a44c6e999d761f8898724090a9a8eb
Eric Polman <eric@ericpolman.com>
2020-04-28 12:23:00 +0200

    Finalized localstorage hook, implemented dark mode

--- submissions/day-002-react-hooks-creation/dark-mode-hook/src/App.tsx
+++ submissions/day-002-react-hooks-creation/dark-mode-hook/src/App.tsx
@@ -1,53 +1,57 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./App.css";
 import { Helmet, HelmetProvider } from "react-helmet-async";
-
-const useLocalState = (
-  key: string,
-  defaultValue: any
-): [string, (newValue: any) => void] => {
-  const [value, setValue] = useState(() => {
-    const storedValue = localStorage.getItem(key);
-    return storedValue !== null ? JSON.parse(storedValue) : defaultValue;
-  });
-
-  useEffect(() => {
-    const listener = (event: StorageEvent) => {
-      if (event.storageArea === localStorage && event.key === key) {
-        setValue(event.newValue !== null ? JSON.parse(event.newValue) : null);
-      }
-    };
-    window.addEventListener("storage", listener);
-
-    return () => {
-      window.removeEventListener("storage", listener);
-    };
-  }, [key]);
-
-  const setValueInLocalStorage = (newValue: any | (() => any)) => {
-    setValue((currentValue: string) => {
-      const result =
-        typeof newValue === "function" ? newValue(currentValue) : newValue;
-      console.log(result);
-      localStorage.setItem(key, JSON.stringify(result));
-      return newValue;
-    });
-  };
-
-  return [value, setValueInLocalStorage];
-};
+import { useLocalState } from "./useLocalState";
 
 function App() {
   const [theme, setTheme] = useLocalState("theme", "light");
 
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-      <h1>{theme}</h1>
-      <button onClick={() => setTheme(theme === "light" ? "dark" : "light")}>
-        Toggle theme
-      </button>
-    </div>
+    <HelmetProvider>
+      <Helmet>
+        <body data-theme={theme} />
+      </Helmet>
+      <div className="App">
+        <h1>{theme === "light" ? "Light Theme" : "Dark Theme"}</h1>
+        <p>
+          Minim quis velit ad minim sit ipsum cillum consectetur fugiat tempor
+          magna. Ea sint tempor sint in sint non proident nostrud quis
+          adipisicing. Id ut irure et exercitation est fugiat sint. Mollit quis
+          eu non cillum ea exercitation laboris excepteur labore. Nisi occaecat
+          nostrud ullamco ipsum ut ipsum cupidatat aute minim duis non laborum
+          ea. Nostrud culpa elit ut id adipisicing sunt voluptate excepteur.
+        </p>
+        <p>
+          Esse minim amet adipisicing eiusmod enim officia incididunt est.
+          Aliqua anim eu consectetur sunt fugiat occaecat ex duis. Aliquip Lorem
+          ut do amet irure veniam. Laborum sunt sit incididunt exercitation
+          exercitation culpa non voluptate velit consequat dolore adipisicing.
+          In ad eiusmod consequat voluptate fugiat exercitation do fugiat.
+        </p>
+        <p>
+          Consequat adipisicing amet officia in. Sunt voluptate esse tempor in
+          deserunt in et irure do do veniam cupidatat ut fugiat. Esse laborum
+          cupidatat in consequat ut veniam labore consectetur adipisicing duis.
+          Cupidatat nulla aute labore mollit ea consequat mollit laboris sit
+          tempor.
+        </p>
+        <p>
+          Exercitation voluptate ex qui amet non officia dolor est excepteur
+          ullamco. Reprehenderit qui aliqua aute anim sit aliquip ut. Magna anim
+          aliqua adipisicing pariatur. Do laboris adipisicing pariatur minim
+          quis sit magna duis velit proident quis sunt ex consectetur. Dolor
+          culpa duis veniam consequat dolore. Irure exercitation duis officia
+          quis officia est. Excepteur elit sit veniam veniam eu voluptate aliqua
+          tempor.
+        </p>
+        <button
+          id="theme-button"
+          onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+        >
+          <img src="flashlight.png" />
+        </button>
+      </div>
+    </HelmetProvider>
   );
 }
 
