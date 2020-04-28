e4f9173d3d0615767e7ec789a43adc7f21e67fe8
Eric Polman <eric@ericpolman.com>
2020-04-28 12:04:02 +0200

    useLocalState hook

--- submissions/day-002-react-hooks-creation/dark-mode-hook/src/App.tsx
+++ submissions/day-002-react-hooks-creation/dark-mode-hook/src/App.tsx
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+
+const useLocalState = (
+  key: string,
+  defaultValue: any
+): [string, (newValue: any) => void] => {
+  const [value, setValue] = useState(() => {
+    const storedValue = localStorage.getItem(key);
+    return storedValue !== null ? JSON.parse(storedValue) : defaultValue;
+  });
+
+  useEffect(() => {
+    const listener = (event: StorageEvent) => {
+      if (event.storageArea === localStorage && event.key === key) {
+        setValue(event.newValue !== null ? JSON.parse(event.newValue) : null);
+      }
+    };
+    window.addEventListener("storage", listener);
+
+    return () => {
+      window.removeEventListener("storage", listener);
+    };
+  }, [key]);
+
+  const setValueInLocalStorage = (newValue: any | (() => any)) => {
+    setValue((currentValue: string) => {
+      const result =
+        typeof newValue === "function" ? newValue(currentValue) : newValue;
+      console.log(result);
+      localStorage.setItem(key, JSON.stringify(result));
+      return newValue;
+    });
+  };
+
+  return [value, setValueInLocalStorage];
+};
 
 function App() {
+  const [theme, setTheme] = useLocalState("theme", "light");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello world</h1>
+      <h1>{theme}</h1>
+      <button onClick={() => setTheme(theme === "light" ? "dark" : "light")}>
+        Toggle theme
+      </button>
     </div>
   );
 }
