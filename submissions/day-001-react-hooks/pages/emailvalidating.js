aebdc528f0cc3eb83cd6a968d6eb78697ad17ce5
Eric Polman <eric@ericpolman.com>
2020-05-03 14:52:54 +0200

    Updates

--- submissions/day-001-react-hooks/pages/emailvalidating.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import EmailValidatingForm from "../src/EmailValidatingForm";
-
-export default function emailvalidating() {
-  return <EmailValidatingForm />;
-}
